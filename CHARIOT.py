8358f008de6eae7084d6269c164db6e42347ab42
Nick Cheng <nickcheng@DESKTOP-RL8O7DU.localdomain>
2024-04-02 09:38:57 -0400

    Access token refresh

--- CHARIOT.py
+++ CHARIOT.py
@@ -1,18 +1,69 @@
-"""Flask app for event handler"""
+"""This Python script provides examples on using the E*TRADE API endpoints
+Trade handler is named after the major arcana CHARIOT which represents control and action
+"""
 from __future__ import print_function
 import webbrowser
 import configparser
 from rauth import OAuth1Service
 from requests_oauthlib import OAuth1
 from accounts.accounts import Accounts
+from datetime import datetime
 import requests
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 
 # loading configuration file
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-OAUTH_REFRESH_MIN = 60 #  from etrade docs, access tokens expire every 2 hours and at midnight by default
+OAUTH_REFRESH_MIN = 90 #  from etrade docs, access tokens expire every 2 hours and at midnight by default
 OAUTH_RETRY_MIN = 15 #  if we fail to refresh, wait this many minutes and retry
+LOGIN_STAGE_WAIT_TIME_SEC = 25
+
+def spoofLogin(website_url):
+	options = Options()
+	options.add_argument("--window-size=1920,1200")
+	driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+	driver.get(website_url)
+
+	user = WebDriverWait(driver, LOGIN_STAGE_WAIT_TIME_SEC).until(
+			EC.visibility_of_element_located((By.ID, "USER"))
+	)
+	pwd = WebDriverWait(driver, LOGIN_STAGE_WAIT_TIME_SEC).until(
+			EC.visibility_of_element_located((By.ID, "password"))
+	)
+	submit = WebDriverWait(driver, LOGIN_STAGE_WAIT_TIME_SEC).until(
+			EC.visibility_of_element_located((By.ID, "mfaLogonButton"))
+	)
+
+	user.send_keys(config["ACCESS"]["UID"])
+	pwd.send_keys(config["ACCESS"]["PWD"])
+	submit.click()
+
+	termsSubmit = WebDriverWait(driver, LOGIN_STAGE_WAIT_TIME_SEC).until(
+			EC.visibility_of_element_located((By.ID, "acceptSubmit"))
+	)
+
+	termsSubmit.click()
+
+	code = WebDriverWait(driver, LOGIN_STAGE_WAIT_TIME_SEC).until(
+			EC.visibility_of_element_located((By.XPATH, "//input"))
+	)
+	if (code):
+			code = code.get_attribute("value")
+			driver.quit()
+			return code
+	else:
+			driver.quit()
+			return None
 
 def oauth():
 	"""Allows user authorization for the sample application with OAuth 1"""
@@ -34,10 +85,8 @@ def oauth():
 	# Step 2: Go through the authentication flow. Login to E*TRADE.
 	# After you login, the page will provide a verification code to enter.
 	authorize_url = etrade.authorize_url.format(etrade.consumer_key, request_token)
-	webbrowser.open(authorize_url)
-	print(authorize_url)
-	text_code = input("Please accept agreement and enter verification code from browser: ")
-
+	text_code = spoofLogin(authorize_url)
+	
 	# Step 3: Exchange the authorized request token for an authenticated OAuth 1 session
 	session = etrade.get_auth_session(request_token,
 									request_token_secret,
--- app.py
+++ app.py
@@ -5,23 +5,20 @@ import sys, os
 sys.path.append(os.getcwd())
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 import CHARIOT
 import datetime
 import time
 
 app = Flask(__name__)
-session, base_url = CHARIOT.oauth()
-
-@app.route('/', methods=['GET'])
-def get_cash():
-	cash = CHARIOT.get_cash_balance(session, base_url)
-	if cash:
-		return jsonify({'cash' : cash})
-	return jsonify({})
+oauth_lock = False
 
 def renew_session_token():
+	if oauth_lock:
+		print("\nOAUTH locked, skipping renew cycle\n")
+
 	today = datetime.now()
-	print('\n', 'Token refresh at ', today.strftime('%Y-%m-%d | %H:%M:%S'), '\n')
+	print('\n', 'TOK Token refresh at ', today.strftime('%Y-%m-%d | %H:%M:%S'), '\n')
 	
 	response = CHARIOT.renew_session(session)
 
@@ -32,9 +29,27 @@ def renew_session_token():
 		if response.status_code != 200:
 			print("Refresh failed twice")
 
+def renew_access_token():
+	oauth_lock = True
+	session, base_url = CHARIOT.oauth()
+	oauth_lock = False
+	print("\nTOK Access token generated\n")
+
+	return session, base_url
+
+session, base_url = renew_access_token()
+
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=renew_session_token, trigger="interval", seconds=CHARIOT.OAUTH_REFRESH_MIN*60)
+scheduler.add_job(func=renew_access_token, trigger=CronTrigger(hour=0, minute=0))
 scheduler.start()
+
+@app.route('/', methods=['GET'])
+def get_cash():
+	cash = CHARIOT.get_cash_balance(session, base_url)
+	if cash:
+		return jsonify({'cash' : cash})
+	return jsonify({})
 
 if __name__ == '__main__':
 	app.run(debug=True, threaded=True)
